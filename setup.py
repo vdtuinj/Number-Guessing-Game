253827541d1e986faa0ddf9ea97892f06e456194
vdtuinj <73612884+vdtuinj@users.noreply.github.com>
2023-07-29 16:57:10 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,7 @@
+from distutils.core import setup
+
+setup(name='NumberGuesser',
+      version='1.0',
+      description='Just a small game',
+      author='vdtuinj',
+     packages=['numberguesser'])
