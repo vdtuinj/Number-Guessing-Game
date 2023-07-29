2baa6c225627ced17f30c68895f5a9c6541b7dca
vdtuinj <73612884+vdtuinj@users.noreply.github.com>
2023-07-29 17:07:08 +0200

    Delete setup.py

--- setup.py
+++
@@ -1,7 +0,0 @@
-from distutils.core import setup
-
-setup(name='NumberGuesser',
-      version='1.0',
-      description='Just a small game',
-      author='vdtuinj',
-     packages=['numberguesser'])
